e6241711828392d37c5527763f83406c8af27611
Sebastien Tricaud <sebastien@honeynet.org>
2020-04-23 09:12:26 -0700

    Adding version

---
+++ python/sightingdb/__version__.py
@@ -0,0 +1,10 @@
+"""Version and details for sightingdb"""
+
+__description__ = "The Python Client Library for SightingDB"
+__url__ = "https://www.github.com/stricaud/sightingdb-client"
+__version__ = "0.0.6"
+__author__ = "Sebastien Tricaud"
+__author_email__ = "sebastien@honeynet.org"
+__license__ = "MIT"
+__maintainer__ = __author__
+__maintainer_email__ = __author_email__
